78b0658959d81e0a7852d32d1b032e33aec05913
linalina07 <linalina077@gmail.com>
2016-03-09 16:54:58 +0900

    9-1 게시판 만들기

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open",function(){
@@ -10,6 +13,64 @@ db.once("open",function(){
 });
 db.on("error",function(err){
   console.log("DB ERROR :", err);
+});
+
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:String, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post',postSchema);
+
+// view setting
+app.set("view engine", 'ejs');
+
+// set middlewares
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+
+// set routes
+app.get('/posts', function(req,res){
+  Post.find({}, function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:posts});
+  });
+}); // index
+
+app.post('/posts', function(req,res){
+  Post.create(req.body.post,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+}); // create
+
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+});
+
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" updated"});
+  });
+});
+
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" deleted"});
+  });
+});
+
+// start Server
+app.listen(3000, function() {
+  console.log('Server On!');
 });
 
 var dataSchema = mongoose.Schema({
@@ -26,9 +87,6 @@ Data.findOne({name:"myData"},function(err,data){
     });
   }
 });
-
-app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
   Data.findOne({name:"myData"},function(err,data){
@@ -74,7 +132,3 @@ function getCounter(res){
     res.render('my_first_ejs', data);
   });
 }
-
-app.listen(3000, function() {
-  console.log('Server On!');
-});
